789f5f527734fd08334e621047d767fa59273ae3
LIDIYA GEZAHEGN DADI <106866923+Lindagez@users.noreply.github.com>
2024-02-07 16:14:44 +0300

    Update

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -25,14 +25,14 @@ app.use(cors());
 // using the APIs
 router.post("/api/products/add", auth, addItem);
 router.put("/api/products/edit/:id", auth, editItem);
-router.delete("/api/products/delete/:id", auth, deleteItem);
+
 router.get("/api/products/", getItems);
 router.get("/api/products/owner/:id", getItemByUserId);
 // router.get("/api/products/:id", getItemById);
-router.get("/api/products/search/", searchAll);
+
 router.get("/api/products/search/:name", searchProducts);
 router.get("/api/products/category/:category", searchProductsByCategory);
-router.get("/api/products/price/:unitPrice", searchProductsByPrice);
+
 router.get("/api/products/sort/", sortProductsByPrice);
 router.get("/api/products/sort/alphabet/", sortProductsByAlphabet);
 router.get("/api/products/sort/date/", sortProductsByDate);
